80f600b7750502723e5b36a72a581a96a888ca54
Kelly Wyss <kelly.wyss@protonmail.com>
2017-03-25 04:40:10 +0000

    Added contact form mailer view

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,11 +1,4 @@
-class ContactMailer < ActionMailer::Base
-  default to: 'kelly.wyss@yahoo.com'
-  
-  def contact_email(name, email, body)
-    @name = name
-    @email = email
-    @body = body
-    
-    mail(from: email, subject: "Contact Form Message")
-  end
+class ApplicationMailer < ActionMailer::Base
+  default from: 'from@example.com'
+  layout 'mailer'
 end
